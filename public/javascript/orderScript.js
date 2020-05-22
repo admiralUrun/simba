d390a5bc12ad7f7438a51be5b032e5cf97296be8
Andrii Yakovenko <admiralurun@gmail.com>
2020-05-22 09:33:11 +0300

    Add simple JS

--- public/javascript/orderScript.js
+++ public/javascript/orderScript.js
@@ -1,3 +1,29 @@
 function addToOrder(item) {
     console.log(item);
+    let order = $('#inOrder')
+    let inOrder = order.val()
+    let isValueEmpty = inOrder === ''
+    if (isValueEmpty) {
+        order.val(item)
+        addElementForDisplay(isValueEmpty)
+    } else {
+        order.val(inOrder + ', ' + item)
+        addElementForDisplay(isValueEmpty)
+    }
+}
+
+function addElementForDisplay(isEmpty) {
+    let order = $('#inOrder')
+    let inOrder = order.val().split(',')
+    if(isEmpty) {
+
+    } else {
+
+    }
+
+    console.log(inOrder.length)
+}
+
+function deleteFromOrder(index) {
+
 }
