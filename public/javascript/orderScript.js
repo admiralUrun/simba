79336039540d7fad76dea0c80a251e97f790bd13
Andrii Yakovenko <admiralurun@gmail.com>
2020-05-22 12:12:34 +0300

    Create draft of adding into order

--- public/javascript/orderScript.js
+++ public/javascript/orderScript.js
@@ -5,25 +5,34 @@ function addToOrder(item) {
     let isValueEmpty = inOrder === ''
     if (isValueEmpty) {
         order.val(item)
-        addElementForDisplay(isValueEmpty)
+        addElementForDisplay(isValueEmpty, item)
     } else {
         order.val(inOrder + ', ' + item)
-        addElementForDisplay(isValueEmpty)
+        addElementForDisplay(isValueEmpty, item)
     }
 }
 
-function addElementForDisplay(isEmpty) {
-    let order = $('#inOrder')
-    let inOrder = order.val().split(',')
-    if(isEmpty) {
-
-    } else {
-
+function addElementForDisplay(isEmpty, text) {
+    function addElementForDisplay(index) {
+        $('<li>'+ text + '<button type="button" class="close" aria-label="Close">\n' +
+            '  <span aria-hidden="true">&times;</span>\n' +
+            '</button></li>\n', {
+            id: 'element' + index,
+            type: 'button',
+            class: 'list-group-item',
+            "onclick": deleteFromOrder(index)
+        }).appendTo('ul')
     }
-
-    console.log(inOrder.length)
+    let order = $('#inOrder')
+    if(isEmpty) {
+        addElementForDisplay(0)
+    } else {
+        let inOrder = order.val().split(',')
+        addElementForDisplay(inOrder.length - 1)
+        console.log(inOrder.length)
+    }
 }
 
 function deleteFromOrder(index) {
-
+console.log('Delete' + index)
 }
