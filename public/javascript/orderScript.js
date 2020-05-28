8fe8947b65f3ff1c59dff4ebedf2adea8a540271
Andrii Yakovenko <admiralurun@gmail.com>
2020-05-28 12:56:00 +0300

    First version of Customer Search for Order

--- public/javascript/orderScript.js
+++ public/javascript/orderScript.js
@@ -1,3 +1,5 @@
+jQuery.exists = function(selector) {return ($(selector).length > 0);}
+jQuery.isDefended = function(object) {return object !== null}
 function addToOrder(item, title) {
     let order = $('#inOrder')
     let inOrder = order.val()
@@ -66,11 +68,57 @@ function changeButton(id, trueLabel, falseLabel, inputID) {
     input.val(!isChecked)
 }
 
-function setCustomerForOrder() {
-    // TODO make a UI changes as hide a search and display Customer ALSO set proper vales!
-}
+function takeJSONFromDateSetCustomerForOrder(customerID) {
+    function setCustomerForOrder(customer) {
+        function formatSting(s) {
+            if($.isDefended(s)) return ' ' + s;
+                else return ''
+            }
+            function addCustomerToOrderAndToUI(c) {
+                function getAFullAddress() {
+                    return c.address + formatSting(c.entrance) + formatSting(c.foolr) + formatSting(c.flat)
+                }
+                $(`#customersID`).val(`${c.id}`)
+                $(`<h4 id="customer">${c.firstName} ${formatSting(c.lastName)} Телефон:${c.phone}  Місто:${c.city} Адреса:${getAFullAddress()}</h4>`) // FOR first time
+                    .appendTo($('#customerInformation'))
+            }
+            function allSearchElementsStyleDisplayNone() {
+                $("#searchDIV").css('display', 'none') //= "display: none"
+            }
+            allSearchElementsStyleDisplayNone()
+            addCustomerToOrderAndToUI(customer)
+    }
+    let json = $(`#${customerID}`).data('customerJSON')
+    console.log(json)
+    setCustomerForOrder(json)
+    }
+
 
 function takeCustomerJSONAddToUI(JSONArray) {
-    console.log("Okay that is works")
-    console.log(JSONArray)
+    // <div id="dropdown-menu" className="dropdown-menu">
+    //     <span className="dropdown-item-text">Dropdown item text</span>
+    //     <a className="dropdown-item" href="#">Action</a>
+    //     <a className="dropdown-item" href="#">Another action</a>
+    //     <a className="dropdown-item" href="#">Something else here</a>
+    // </div>
+    function addDropItem(Customer) {
+        let elementExists = document.getElementById(`customer${Customer.id}`);
+        if(elementExists) {}
+        else {
+            $(`<a id="customer${Customer.id}" class="dropdown-item start" href="#" onclick="takeJSONFromDateSetCustomerForOrder(\`customer${Customer.id}\`)">${Customer.firstName} ${Customer.lastName} ${Customer.address}</a>`)
+                .appendTo($("#searchMenu")
+            )
+            $(`#customer${Customer.id}`).data('customerJSON', Customer)
+        }
+    }
+    JSONArray.forEach(function (c) {
+        addDropItem(c)
+    })
+    // console.log("Okay that is works")
+    // console.log(JSONArray)
+}
+
+function cleanDropMenu() {
+    document.getElementById('searchMenu').innerHTML = ''
+ $(`<span class="dropdown-header">Тут можна знайти Клієнта</span>`).appendTo($("#searchMenu"))
 }
