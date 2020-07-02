b1900209d6c0d44a0373e9dccfa2950c7b9cc1e1
Andrii Yakovenko <admiralurun@gmail.com>
2020-07-02 10:43:41 +0300

    First version for search and set Customer with multiple addresses Changed bootstrap.min to bootstrap in main.scala.html

--- public/javascript/orderScript.js
+++ public/javascript/orderScript.js
@@ -2,9 +2,23 @@ jQuery.exists = function (selector) {
     return ($(selector).length > 0);
 }
 jQuery.isDefended = function (object) {
-    return object !== null
+    return object != null
+}
+jQuery.isEmpty = function (object) {
+    return object == null
+}
+jQuery.getValeOrEmptyString = function(s) {
+    if ($.isDefended(s)) return ' ' + s;
+    else return ''
+}
+jQuery.selectorToHidden = function (selector) {
+    $(selector).hide()
+}
+jQuery.insertData = function (selector, key, value) {
+    $(selector).data(key, value)
 }
 
+let customerInfoDiv = $("#customerInformation")
 function addToOrder(item, title, cost) {
     function addElementForDisplay(isEmpty, text, cost) {
         function addElementForDisplay(index) {
@@ -28,7 +42,6 @@ function addToOrder(item, title, cost) {
         order.val(inOrder + ', ' + item)
         changeTotal(Number(cost))
     }
-    console.log(order.val())
     addElementForDisplay(isValueEmpty, title, cost)
 }
 
@@ -93,46 +106,35 @@ function changeButton(trueLabel, falseLabel, inputID, cost) {
     input.val(!isChecked)
 }
 
-function takeJSONFromDateSetCustomerForOrder(customerID) {
-    function setCustomerForOrder(customer) {
-        function getValeOrEmptyString(s) {
-            if ($.isDefended(s)) return ' ' + s;
-            else return ''
-        }
-
-        function addCustomerToOrderAndToUI(c) {
-            function getAFullAddress() {
-                return c.address + getValeOrEmptyString(c.entrance) + getValeOrEmptyString(c.floor) + getValeOrEmptyString(c.flat)
-            }
-
-            $(`#customersID`).val(`${c.id}`)
-            $(`<h4 id="customer">${c.firstName} ${getValeOrEmptyString(c.lastName)} Телефон:${c.phone}  Місто:${c.city} Адреса:${getAFullAddress()}</h4>
-                    <button id="editButton" class="btn btn-success" onclick="editCustomerInOrder()">Знайти іншого</button>`) // FOR first time
-                .appendTo($('#customerInformation'))
-        }
-
-        function allSearchElementsStyleDisplayNone() {
-            $("#searchDIV").hide()
-        }
-
-        allSearchElementsStyleDisplayNone()
-        addCustomerToOrderAndToUI(customer)
-    }
-
-    let json = $(`#${customerID}`).data('customerJSON')
-    setCustomerForOrder(json)
-}
-
 function takeCustomerJSONAddToUI(JSONArray) {
-    function addDropItem(Customer) {
-        let elementExists = document.getElementById(`customer${Customer.id}`);
-        if (elementExists) {
-        } else {
-            $(`<a id="customer${Customer.id}" class="dropdown-item start" href="#" onclick="takeJSONFromDateSetCustomerForOrder(\`customer${Customer.id}\`)">${Customer.firstName} ${Customer.lastName} ${Customer.address}</a>`)
-                .appendTo($("#searchMenu")
-                )
-            $(`#customer${Customer.id}`).data('customerJSON', Customer)
+    function addDropItem(customerAddresses) {
+        function addDropItem() {
+            function gettingDropDownItems(array) {
+                let dropDowns = array.map(function (address) {
+                    return `<a class="dropdown-item" onclick="setAddress( '${address.id}')"
+                        >${address.city} ${address.residentialComplex} ${address.address} ${address.entrance} ${address.floor} ${address.flat}</a>`
+                })
+                return dropDowns.join(`\n`)
+            }
+            if (customerAddresses.addresses.length > 1) {
+                $(`<a id="customer${customerAddresses.customer.id}" class="dropdown-item" href="#"
+                    onclick="setCustomerCreateDropDownFormAddresses('${customerAddresses.customer.id}')"
+                    >${customerAddresses.customer.firstName} ${customerAddresses.customer.phone} ${$.getValeOrEmptyString(customerAddresses.customer.instagram)}
+                    </a>`
+                ).appendTo($("#searchMenu"))
+            } else if (customerAddresses.addresses.length !== 0) {
+                $(`<a id="customer${customerAddresses.customer.id}" class="dropdown-item" href="#"
+                    onclick="setCustomerAndAddress('${customerAddresses.customer.id}', '${customerAddresses.addresses[0].id}')"
+                    >${customerAddresses.customer.firstName} ${customerAddresses.customer.phone} ${$.getValeOrEmptyString(customerAddresses.customer.instagram)}
+                    </a>`
+                ).appendTo($("#searchMenu"))
+            } else {}
+            $(`#customer${customerAddresses.customer.id}`).data('customerJSON', customerAddresses)
         }
+
+        if (!$.exists(`#customer${customerAddresses.customer.id}`)) {
+            addDropItem()
+        } else {}
     }
 
     JSONArray.forEach(function (c) {
@@ -140,10 +142,144 @@ function takeCustomerJSONAddToUI(JSONArray) {
     })
 }
 
+function displayCustomer(customer) {
+    function getPhones(customer) {
+        function secondPhone(phone, note) {
+            if($.isEmpty(phone) || $.isEmpty(note)) return ''
+            else return " " + `Другий Телефон:` + phone + $.getValeOrEmptyString(note)
+        }
+        return `Телефони:` + customer.phone + ` ` + $.getValeOrEmptyString(customer.phoneNote) + secondPhone(customer.phone2, customer.phoneNote2)
+    }
+    $(`
+          <div id="customer" class="col-auto">
+            <dl class="col-auto">
+                ${descriptionListElement('Ім\'я', customer.firstName + ' ' + $.getValeOrEmptyString(customer.lastName))}
+                ${descriptionListElement('Телефони', getPhones(customer))}
+                ${descriptionListElement('Instagram', $.getValeOrEmptyString(customer.instagram))}
+                ${descriptionListElement('Нотатка', $.getValeOrEmptyString(customer.notes))}
+            </dl>
+           <div class="col">
+                <button id="editCustomerButton" class="btn btn-success" onclick="editCustomerInOrder()">Знайти іншого</button>
+           </div>
+           </div>
+        `).appendTo(customerInfoDiv)
+}
+
+function displayAddress(address, thereIsOnlyOneAddress) {
+    $(`        
+          <div id="address" class="col-auto">
+            <dl class="col-auto">
+                <div class="row">
+                    ${descriptionListElement("Місто", address.city, true)}
+                    ${descriptionListElement("ЖК", address.residentialComplex, true)}
+                </div>
+            ${descriptionListElement("Адреса", address.address)}
+                <div class="row">
+                    ${descriptionListElement('Під\'їзд', address.entrance, true)}
+                    ${descriptionListElement('Поверх', address.floor, true)}
+                    ${descriptionListElement('Квартира', address.flat, true)}
+                </div>
+            ${descriptionListElement('Нотатка', address.notesForCourier)}
+          </dl>
+          </div>
+        `).appendTo(customerInfoDiv)
+}
+
+function setCustomerCreateDropDownFormAddresses(id) {
+    function setCustomerCreateDropDownFormAddresses(customerAddresses) {
+        function createDropDownForAddresses(addresses) {
+            $(`
+           <div id="addresses" class="col-auto btn-group dropright">
+            <div>
+                <button type="button" class="btn btn-info start dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Адреси 
+                </button>
+            <div id="customer${customerAddresses.id}Addresses" class="dropdown-menu"></div>
+            </div>
+            </div>`
+            ).appendTo($('#customerInformation'))
+            addresses.forEach( function (address) {
+                $(`<a id="address${address.id}" class="dropdown-item" onclick="setAddress(null, false, '${address.id}')">
+                            ${address.city} ${$.getValeOrEmptyString(address.residentialComplex)}
+                            ${address.address}
+                            ${$.getValeOrEmptyString(address.flat)}
+                        </a>`).appendTo($(`#customer${customerAddresses.id}Addresses`))
+                $(`#address${address.id}`).data(`address`, address)
+                }
+            )
+        }
+
+        $.selectorToHidden("#searchDIV")
+        setCustomer(customerAddresses.customer)
+        createDropDownForAddresses(customerAddresses.addresses)
+    }
+
+    setCustomerCreateDropDownFormAddresses($(`#customer${id}`).data(`customerJSON`))
+}
+
+function setAddress(address, thereIsOnlyOneAddress, id) {
+    function setAddress(address, thereIsOnlyOneAddress) {
+        $(`#address`).remove()
+        $(`#addressID`).val(`${address.id}`)
+        displayAddress(address, thereIsOnlyOneAddress)
+    }
+
+    if($.isDefended(address)) setAddress(address, thereIsOnlyOneAddress)
+    else setAddress($(`#address${id}`).data(`address`), thereIsOnlyOneAddress)
+}
+function setCustomer(customer) {
+    $(`#customersID`).val(`${customer.id}`)
+    displayCustomer(customer)
+}
+
+function setCustomerAndAddress(customerID, addressID) {
+    function setCustomerForOrder(customerAddress) {
+        function addCustomerAddressToOrderAndToUI(customer, address) {
+            setCustomer(customer)
+            setAddress(address, true)
+        }
+
+        addCustomerAddressToOrderAndToUI(customerAddress.customer, customerAddress.addresses[0])
+        $.selectorToHidden("#searchDIV")
+    }
+
+    let json = $(`#customer${customerID}`).data('customerJSON')
+    setCustomerForOrder(json)
+}
+
+function descriptionListElement(head, content, needDivWithCol) {
+    if($.isEmpty(head) || $.isEmpty(content) || head === '' || content === '') return ''
+    else {
+        if(needDivWithCol) {
+            return `
+                <div class="col">
+                <dt>
+                    ${head}
+                </dt>
+                <dd>
+                    ${content}
+                </dd>
+                </div>
+            `
+        } else {
+            return`
+                <dt>
+                    ${head}
+                </dt>
+                <dd>
+                    ${content}
+                </dd>
+            `
+        }
+    }
+}
+
 function editCustomerInOrder() {
     $(`#customersID`).val("")
     $(`#customer`).remove()
-    $(`#editButton`).remove()
+    $('#address').remove()
+    $('#addresses').remove()
+    $(`#editCustomerButton`).remove()
     $("#searchDIV").show()
 }
 
